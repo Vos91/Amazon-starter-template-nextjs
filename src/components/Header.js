a9cdabbea7485f2a8d6d1b9c013c44e73d626ac1
Jasper Vos <jasper@mediasolutions>
2022-12-12 13:34:55 +0100

    Second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,9 +28,47 @@ function Header() {
           />
           <SearchIcon className='h-12 p-4' />
         </div>
+
+        {/* Right */}
+        <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+          <div className='link'>
+            <p>Hello Jasper Vos</p>
+            <p className='font-extrabold md:text-sm'>Account &amp; Lists</p>
+          </div>
+
+          <div className='link'>
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>&amp; Orders</p>
+          </div>
+
+          <div className='relative link flex items-center '>
+            <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
+              0
+            </span>
+
+            <ShoppingCartIcon className='h-10' />
+            <p className='hidden md:inline font-extrabold md:text-sm mt-2'>
+              Basket
+            </p>
+          </div>
+        </div>
       </div>
       {/* Bottom nav */}
-      <div></div>
+      <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
+        <p className='link flex items-center'>
+          <MenuIcon className='h-6 mr-1' />
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Business</p>
+        <p className='link'>Today's Deals</p>
+        <p className='link hidden lg:inline-flex'>Electronics</p>
+        <p className='link hidden lg:inline-flex'>Food &amp; Grocery</p>
+        <p className='link hidden lg:inline-flex'>Prime</p>
+        <p className='link hidden lg:inline-flex'>Buy Again</p>
+        <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
+        <p className='link hidden lg:inline-flex'>Health &amp; Personal Care</p>
+      </div>
     </header>
   );
 }
