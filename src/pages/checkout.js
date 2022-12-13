5cb744d0056b6519b0de7fccf83347ea289457a9
Jasper Vos <jasper@mediasolutions>
2022-12-13 15:25:42 +0100

    secret key added

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,24 @@
 import Header from '../components/Header';
+import Image from 'next/image';
 
 function Checkout() {
   return (
     <div className='bg-gray-100'>
       <Header />
+      <main className='lg:flex max-w-screen-2xl mx-auto'>
+        {/* Left */}
+        <div>
+          <Image
+            src='https://links.papareact.com/ikj'
+            width={1020}
+            height={250}
+            objectFit='contain'
+          />
+        </div>
 
-      <h1>CHECKOUT PAGE</h1>
+        {/* Right */}
+        <div></div>
+      </main>
     </div>
   );
 }
