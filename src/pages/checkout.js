4754836eae3b8504ff255ecd7a3a236c096794ac
Jasper Vos <jasper@mediasolutions>
2022-12-15 10:49:02 +0100

    stripe koppeling begin

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,12 +5,15 @@ import Image from 'next/image';
 import { useSelector } from 'react-redux';
 import { selectItems, selectTotal } from '../slices/basketSlice';
 import { useSession } from 'next-auth/react';
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe();
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const { data: session } = useSession();
-  console.log(session);
+
+  const createCheckoutSession = () => {};
 
   return (
     <div className='bg-gray-100'>
@@ -67,6 +70,8 @@ function Checkout() {
                 </span>
               </h2>
               <button
+                role='link'
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
